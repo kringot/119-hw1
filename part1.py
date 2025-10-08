7597cfa1d5bc3aa13c2fcb307c161bb1e923884a
kringot <kringo@ucdavis.edu>
2025-10-08 14:50:19 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -97,6 +97,7 @@ def load_input():
     # columns we are interested in using the NEW_COLUMNS variable above.
     # Make sure you return the columns in the new order.
     # TODO
+    df_2019[:;
 
     # When you are done, remove the next line...
     raise NotImplementedError
