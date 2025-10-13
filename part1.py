ecec4685eb359fec856cba39017aabe63e669a14
kringot <kringo@ucdavis.edu>
2025-10-13 15:40:27 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -322,7 +322,7 @@ def q5b(dfs):
 
     non_null_values = dfs[2].count()
 
-    return [non_null_values]
+    return [non_null_values[1]]
 
 """
 5c.
@@ -332,9 +332,9 @@ We will use this in the unit tests below.
 """
 
 def q5c():
-    raise NotImplementedError
+    # raise NotImplementedError
     # TODO: fill this in with the expected number
-    num_non_null = 0
+    num_non_null = 100
     return num_non_null
 
 """
