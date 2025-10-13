12142001a25ee6b4576bc790a416f0b913f7ebf4
kringot <kringo@ucdavis.edu>
2025-10-13 15:56:55 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -334,7 +334,7 @@ We will use this in the unit tests below.
 def q5c():
     # raise NotImplementedError
     # TODO: fill this in with the expected number
-    num_non_null = 1600
+    num_non_null = 100
     return num_non_null
 
 """
