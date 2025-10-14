88374e1d059037e460ac7bffbeb6f14e0f3f5b3c
kringot <kringo@ucdavis.edu>
2025-10-14 16:17:04 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -800,21 +800,21 @@ def q15_helper(dfs):
 
     # get top 10 universities from 2019, only keep the university and overall score
     df_2019 = dfs[0]
-    top_10_2019_fetus = df_2019[df_2019['rank'] <= 10][['unviersity', 'overall score']].copy()
+    top_10_2019_fetus = df_2019[df_2019['rank'] <= 10][['university', 'overall score']].copy()
 
     # rename the columns
     top_10_2019 = top_10_2019_fetus.rename(columns = {'overall score': 'overall score 2019'})
 
     # get top 10 universities from 2020, only keep the university and overall score
     df_2020 = dfs[1]
-    top_10_2020_fetus = df_2020[df_2020['rank'] <= 10][['unviersity', 'overall score']].copy()
+    top_10_2020_fetus = df_2020[df_2020['rank'] <= 10][['university', 'overall score']].copy()
 
     # rename the columns
     top_10_2020 = top_10_2020_fetus.rename(columns = {'overall score': 'overall score 2020'})
 
     # get top 10 unversities from 2021, only keep the university and overall score
     df_2021 = dfs[2]
-    top_10_2021_fetus = df_2021[df_2021['rank'] <= 10][['unviersity', 'overall score']].copy()
+    top_10_2021_fetus = df_2021[df_2021['rank'] <= 10][['university', 'overall score']].copy()
 
     # rename the columns
     top_10_2021 = top_10_2021_fetus.rename(columns = {'overall score': 'overall score 2021'})
