46bd68d50c73fe40113166bc5d04e3c448bb1d0c
kringot <kringo@ucdavis.edu>
2025-10-15 00:26:39 -0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -1049,12 +1049,13 @@ def q20a(dfs):
     # create row for Berkely
     Berkely_cheat = pd.DataFrame({'rank': [1], 
                                   'university': ['UC Berkely'], 
-                                  'region': ['USA'], 
+                                  'overall score': [0.0],
                                   'academic reputation': [0.0], 
                                   'employer reputation': [0.0], 
                                   'faculty student': [100.0], 
                                   'citations per faculty': [100.0], 
-                                  'overall score': [0.0]})
+                                  'region': ['USA'], 
+                                  'year': ['2021']})
 
     # use .concat to add in new row
     df_2021_cheat = pd.concat([df_2021, Berkely_cheat])
