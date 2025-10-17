823219e4c77e8bea929929cdd98244da36e4829f
kringot <kringo@ucdavis.edu>
2025-10-17 09:49:27 -0700

    Update part3.py

--- part3.py
+++ part3.py
@@ -355,29 +355,10 @@ def pipeline_shell():
     # 'type displays the file content
     # | find /c /v "" counts every line
     shell_command = f"findstr /R /C:^ {POP_FILE} | find /C /V \"\""
-    shell_command_simple = f'find /C /V "" "{POP_FILE}"'
+    shell_command_simple = f'findstr /N /R "^" "{POP_FILE}"'
 
     # execute command and read output string
     output = os.popen(shell_command_simple).read()
-
-    # strip non-digit characters to get clean output
-    # try:
-        # attempt simple conversion first
-        # return int(output.strip())
-    # except ValueError:
-        # filter non_digit characters
-        # cleaned_output = ''.join(filter(str.isdigit, output))
-
-        # if cleaned_output:
-            # return int(cleaned_output)
-        # else:
-            # if command failed, raise rror
-            # print(f"Shell command failed.")
-            # raise ValueError(f"Invalid output from shell command: '{output.strip()}'")
-
-    # strip any leading or trailing whitespace
-    # Return resulting integer
-    # return int(output.strip())
 
     # strip number
     parts = output.strip().split(':')
